bcf2809451ab7f52da461856c0d975155b03f652
f1shl3gs <fishlegs.engerman@gmail.com>
2026-08-29 16:37:01 +0800

    simplify

--- giraffe/src/utils/hasPlotEnv.ts
+++ giraffe/src/utils/hasPlotEnv.ts
@@ -3,7 +3,9 @@ import {Config, LayerTypes} from '../types'
 export const hasPlotEnv = (config: Config): boolean => {
   const type = config.layers[0].type
 
-  return !(type === LayerTypes.Gauge ||
+  return !(
+    type === LayerTypes.Gauge ||
     type === LayerTypes.SimpleTable ||
-    type === LayerTypes.Table);
+    type === LayerTypes.Table
+  )
 }
