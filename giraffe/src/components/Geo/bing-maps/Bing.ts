0a115f84800cf6c96213f68618418ea5733c09e5
f1shl3gs <fishlegs.engerman@gmail.com>
2026-08-29 15:08:24 +0800

    update sass and typescript

--- giraffe/src/components/Geo/bing-maps/Bing.ts
+++ giraffe/src/components/Geo/bing-maps/Bing.ts
@@ -1,12 +1,7 @@
-import PropTypes from 'prop-types'
 import {GridLayer, withLeaflet} from 'react-leaflet'
 import {BingLayerObject} from './BingLayerObject'
 
 class BingLayer extends GridLayer {
-  static propTypes = {
-    bingkey: PropTypes.string.isRequired,
-  }
-
   createLeafletElement(props) {
     return new BingLayerObject(props.bingkey, super.getOptions(props))
   }
