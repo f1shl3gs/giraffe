0c9f4e8fb13b7c563e8eb72e68bf480287f93c99
f1shl3gs <fishlegs.engerman@gmail.com>
2026-09-01 05:26:57 +0800

    simplify

--- giraffe/src/utils/getTicks.ts
+++ giraffe/src/utils/getTicks.ts
@@ -36,15 +36,10 @@ const hasMinimumSpacing = (
   const totalLength = ticks.length * timeTickLength
   if (ticks.length < 4) {
     const padding = totalLength * fractionalSpaceAsPadding
-    if (totalLength < rangeLength - padding) {
-      return true
-    }
-    return false
+    return totalLength < rangeLength - padding;
   } else if (ticks.length <= 10) {
-    if (totalLength < rangeLength - timeTickLength) {
-      return true
-    }
-    return false
+    return totalLength < rangeLength - timeTickLength;
+
   } else if (
     totalLength <
     rangeLength - Math.ceil(ticks.length / 10) * timeTickLength
